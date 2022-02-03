02679642f6750532a86fdbfd2745c1a216303222
MiqSA <eng.miqueias.sa@gmail.com>
2022-02-03 00:02:24 -0300

    Update simulations with create, loop and results working

--- app/main/resources/simulations.py
+++ app/main/resources/simulations.py
@@ -1,55 +1,47 @@
-from app.main.services.config_print import printProgressBar, bcolors
+from app.main.services.config_print import bcolors,printProgressBar
 from app.main.controllers.games_controller import BankGame
-from app.main.services.manager_files import JsonFiles
-from app.main.config import PATH_FILES, FILENAME_RESULTS
+from app.main.controllers.simulation_controller import OutputSimulation
+from app.main.config import ITERATIONS, TIMEOUT
+import time
 
 
 
-
-
-class Simulation():
-    def __init__(self, parameters):
-        self.game = parameters.get('game')
-        self.iterations = parameters.get('iterations', 300)
-        self.timeout = parameters.get('timeout', 1000)
-
-    @classmethod
-    def initialize(cls):
-        files = JsonFiles(PATH_FILES, FILENAME_RESULTS)
-        if files.read() is False:
-            files.new()
-        files.clear()
+class Simulation:
+    def __init__(self):
+        self.iterations = ITERATIONS
+        self.timeout = TIMEOUT
 
 
     def create(self):
         try:
-            self.initialize()
+            OutputSimulation.initialize()
             BankGame.initialize()
-
-            print(f"{bcolors.HEADER} Created enviroment simulation game with success! {bcolors.ENDC} \n")
-
         except Exception as err:
             print(f"{bcolors.WARNING} Error Dont created simulation game! {bcolors.ENDC}")
 
     def loop(self):
         try:
+            printProgressBar(0, self.iterations, prefix='Progress:', suffix='Complete', length=50)
             for iteration in range(self.iterations):
-                print(f"{bcolors.OKCYAN}> {iteration+1}. Simulation{bcolors.ENDC}")
-
 
                 BankGame.new_match()
-                BankGame.sort_players()
-                # BankGame.play(self.timeout)
+                sorted_players = BankGame.sort_players()
+                BankGame.play(self.timeout, sorted_players)
+
+                time.sleep(0.1)
+
+                # Update Progress Bar
+                printProgressBar(iteration + 1, self.iterations, prefix='Progress:', suffix='Complete', length=50)
 
         except Exception as err:
-            print('Loop doesnt work!',err)
+            print(f"{bcolors.WARNING} Loop doesnt work!! {bcolors.ENDC}", err)
 
     def results(self):
         try:
-            print(f"{bcolors.OKGREEN}>>>> Simulation results <<<<{bcolors.ENDC}")
+            print(f"{bcolors.OKGREEN}>>>> Resultados da Simulação <<<<{bcolors.ENDC}")
+            OutputSimulation.show_results()
         except Exception as err:
-            print('Error in imulation results!', err)
-
+            print('Error in simulation results!', err)
 
     def run(self):
         self.create()
