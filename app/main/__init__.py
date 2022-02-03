d8779f921766b19ed2e4a40c7325f5130e60e101
MiqSA <eng.miqueias.sa@gmail.com>
2022-02-02 23:48:03 -0300

    Update main init app

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,17 +1,9 @@
 from app.main.resources.simulations import Simulation
-from app.main.controllers.games import BankGame
 
 
 
 def game_bank_simulation():
-    parameters = {
-        'game': BankGame(),
-        'iterations': 2,
-        'timeout': 2
-    }
-    Simulation(parameters).run()
-
-
+    Simulation().run()
 
 
 
