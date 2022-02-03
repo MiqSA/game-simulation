26cc584a33226d8c510caf4980115305aaebddbe
MiqSA <eng.miqueias.sa@gmail.com>
2022-02-02 23:58:23 -0300

    Update players model

--- app/main/models/players.py
+++ app/main/models/players.py
@@ -9,9 +9,10 @@ class PlayerModel:
     def parameters(cls):
         parameters = {
             'id': None,
-            'complete_way': None,
-            'balance_value': None,
-            'profile': None
+            'position': None,
+            'balance_value': 0,
+            'profile': None,
+            'activate': 1 # 0 is False and 1 and True
         }
         return parameters
 
@@ -28,68 +29,36 @@ class PlayerModel:
     def first_conditions(cls):
         files = JsonFiles(PATH_FILES, FILENAME_PLAYERS)
         data = []
-
-        for position in range(TOTAL_PLAYERS):
+        for position in range(1, TOTAL_PLAYERS+1):
             parameters = PlayerModel.parameters()
 
             parameters['id'] = position
             parameters['profile'] = position
+            parameters['balance_value'] = 300
 
             data.append(parameters)
-
-
         files.new(data=data)
 
 
+    @classmethod
+    def read_player(cls, id):
+        files = JsonFiles(PATH_FILES, FILENAME_PLAYERS)
+        all_players = files.read()
+        player = all_players[id-1]
+        return player
 
 
-class PlayersProfiles:
-    def type_1(self):
-        pass
-
-    def type_2(self):
-        pass
-
-    def type_3(self):
-        pass
-
-    def type_4(self):
-        pass
-
-    def get(self, profile_id):
-        profiles = {
-            0: self.type_1(),
-            1: self.type_2(),
-            2: self.type_3(),
-            3: self.type_4()
-        }
-
-        return profiles[profile_id]
+    @classmethod
+    def read_players(cls):
+        files = JsonFiles(PATH_FILES, FILENAME_PLAYERS)
+        all_players = files.read()
+        return all_players
 
 
 
-
-
-
-
-class PlayersInformation:
-    def __init__(self, path, filename):
-        self.path = path
-        self.filename = filename
-
-
-    def new(self):
-        pass
-
-    def list(self):
-        pass
-
-    def update(self, id):
-        pass
-
-    def delete(self, id):
-        pass
-
-
-
-
+    @classmethod
+    def update_player(cls, id, update_data):
+        files = JsonFiles(PATH_FILES, FILENAME_PLAYERS)
+        all_players = files.read()
+        all_players[id-1] = update_data
+        files.new(data=all_players)
