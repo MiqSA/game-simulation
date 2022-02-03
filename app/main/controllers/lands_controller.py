99ef98ddfaaa821721c1465cf951ef236afeb27d
MiqSA <eng.miqueias.sa@gmail.com>
2022-02-02 23:55:03 -0300

    Update lands controller with all functions dynamic

--- app/main/controllers/lands_controller.py
+++ app/main/controllers/lands_controller.py
@@ -11,4 +11,22 @@ class Lands:
     @classmethod
     def first_conditions(cls):
         LandModel.first_conditions()
-        print(f"{bcolors.OKCYAN}      Update parameters to all lands - First conditions {bcolors.ENDC}")
+
+    @classmethod
+    def check_land_is_available(cls, id):
+        land = LandModel.read_lands(id)
+        if land['owner'] is None:
+            return True
+        else:
+            return False
+
+    @classmethod
+    def get_land(cls,id):
+        land = LandModel.read_lands(id)
+        return land
+
+    @classmethod
+    def update_land(cls, id, update_data):
+        LandModel.update_land(id, update_data)
+
+
